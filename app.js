87835f28d87d017a3fea5c56c229afc056a67dc3
Sibil <sibil.sorenofficial@gmail.com>
2021-12-08 13:09:39 +0530

    Required end point created

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("End point working");
+const studentList = ["Rajesh", "Ramesh", "Sayali", "Sanjana"];
+
+app.get("/student/studentList", (req, res) => {
+    res.setHeader("Content-Type", "Application/json");
+    res.send(JSON.stringify({ result: studentList }));
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
